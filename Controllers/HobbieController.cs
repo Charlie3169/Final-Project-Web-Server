4f588f3b2671c175ecb149dda8c054554a12e138
Charlie3169 <77686049+Charlie3169@users.noreply.github.com>
2022-12-02 13:47:40 -0500

    Fixed controllers

---
+++ Controllers/HobbieController.cs
@@ -0,0 +1,31 @@
+﻿using Final_Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Final_Project.Controllers
+{
+    public class HobbieController : Controller
+    {
+        private readonly ILogger<HobbieController> _logger;
+
+        public HobbieController(ILogger<HobbieController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Hobbie1()
+        {
+            return View();
+        }
+
+        public IActionResult Hobbie2()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
